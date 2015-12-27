0f0332dd30a98f6eaf011d5afe466c41103d01a6
Aaron W. Knight <aaron@phrasemix.com>
2015-12-27 11:07:04 -0500

    Update gem version

--- lib/jquery/modal/rails/version.rb
+++ lib/jquery/modal/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Modal
     module Rails
-      VERSION = "0.0.4"
-      JQUERY_MODAL_VERSION = "0.5"
+      VERSION = "0.6.1"
+      JQUERY_MODAL_VERSION = "0.6"
     end
   end
 end
